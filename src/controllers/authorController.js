c70ddb4478948d986de4fd0b25a9c3a6f1f3f640
Atul Singh Parihar <109425665+comrade31@users.noreply.github.com>
2022-11-18 11:22:18 +0530

    Update authorController.js

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -59,7 +59,7 @@ const login = async function (req, res) {
       if (email && password) {
           const author = await authorModel.findOne({ email: email, password: password })
           if (author) {
-             const token = jwt.sign({ author: author._id },"BlogProject")
+             const token = jwt.sign({ authorId: author._id },"BlogProject")
               return res.status(200).send({ status: true, token: token })
           }
           else {
